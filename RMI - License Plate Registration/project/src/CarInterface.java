bb186532fedf21f5b3de8f0d8a5679978e4d21eb
TL <72099370+terylla@users.noreply.github.com>
2020-12-06 02:45:51 -0500

    Update CarInterface.java

--- RMI - License Plate Registration/project/src/CarInterface.java
+++ RMI - License Plate Registration/project/src/CarInterface.java
@@ -1,10 +1,3 @@
-/////////////////////////////////////////////////
-//  Name: Tran Ngoc La (Teryl)
-//  SID: 150353175
-//  Fall 2020 JAC444NBB
-//  Workshop 10
-//  Professor Reza Khotasjeh
-/////////////////////////////////////////////////
 
 import java.rmi.Remote;
 
